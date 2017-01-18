f38e696f71ec04ab53facb97146d11efec9abef0
Júlio Cotta <juliocbcotta@gmail.com>
2017-01-18 01:14:42 -0200

    adds basic ktor gradle project

---
+++ src/main/kotlin/com/example/Main.kt
@@ -0,0 +1,17 @@
+package com.example
+
+import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.features.*
+import org.jetbrains.ktor.logging.*
+import org.jetbrains.ktor.response.*
+import org.jetbrains.ktor.routing.*
+
+fun Application.main() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing) {
+        get("/") {
+            call.respondText("Hello, World!")
+        }
+    }
+}
