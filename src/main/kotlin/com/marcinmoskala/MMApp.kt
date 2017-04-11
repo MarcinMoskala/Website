b6e4ba555400cdf57b1e0d143caaf4d2bb2be471
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-11 13:13:16 +0200

    Move to Netty (to enable Heroku)

--- src/main/kotlin/com/marcinmoskala/MMApp.kt
+++ src/main/kotlin/com/marcinmoskala/MMApp.kt
@@ -1,40 +1,44 @@
 package com.marcinmoskala
 
 import com.marcinmoskala.presentation.indexRoute
+import com.marcinmoskala.presentation.learningRoute
 import com.marcinmoskala.presentation.projectsRoute
 import com.marcinmoskala.presentation.skillsRoute
-import com.marcinmoskala.presentation.learningRoute
 import freemarker.cache.ClassTemplateLoader
 import org.jetbrains.ktor.application.Application
 import org.jetbrains.ktor.application.install
 import org.jetbrains.ktor.content.serveFileSystem
 import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.freemarker.FreeMarker
+import org.jetbrains.ktor.host.embeddedServer
 import org.jetbrains.ktor.locations.Locations
 import org.jetbrains.ktor.logging.CallLogging
+import org.jetbrains.ktor.netty.Netty
 import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.route
 import java.io.File
 
-class MMApp {
-
-    fun Application.install() {
-        install(DefaultHeaders)
-        install(CallLogging)
-        install(FreeMarker) {
-            templateLoader = ClassTemplateLoader(MMApp::class.java.classLoader, "templates")
-        }
-
-        install(Locations)
-        install(Routing) {
-            indexRoute()
-            skillsRoute()
-            projectsRoute()
-            learningRoute()
-            route("static") {
-                serveFileSystem(File("src/main/resources/static"))
-            }
-        }
+fun Application.install() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(environment.classLoader, "templates")
     }
 
+    install(Locations)
+    install(Routing) {
+        indexRoute()
+        skillsRoute()
+        projectsRoute()
+        learningRoute()
+        route("static") {
+            serveFileSystem(File("src/main/resources/static"))
+        }
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val port = Integer.valueOf(System.getenv("PORT") ?: "8080")
+    embeddedServer(Netty, port, reloadPackages = listOf("heroku"), module = Application::install).start()
 }
