3cccbacdb134a0b6d97fe8283b115b5344879648
Marcin Moskala <marcinmoskala@gmail.com>
2017-04-11 13:46:35 +0200

    Move back to jetty

--- src/main/kotlin/com/marcinmoskala/MMApp.kt
+++ src/main/kotlin/com/marcinmoskala/MMApp.kt
@@ -10,35 +10,31 @@ import org.jetbrains.ktor.application.install
 import org.jetbrains.ktor.content.serveFileSystem
 import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.freemarker.FreeMarker
-import org.jetbrains.ktor.host.embeddedServer
 import org.jetbrains.ktor.locations.Locations
 import org.jetbrains.ktor.logging.CallLogging
-import org.jetbrains.ktor.netty.Netty
 import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.route
 import java.io.File
 
-fun Application.install() {
-    install(DefaultHeaders)
-    install(CallLogging)
-    install(FreeMarker) {
-        templateLoader = ClassTemplateLoader(environment.classLoader, "templates")
-    }
+class MMApp {
 
-    install(Locations)
-    install(Routing) {
-        indexRoute()
-        skillsRoute()
-        projectsRoute()
-        learningRoute()
-        route("static") {
-            serveFileSystem(File("src/main/resources/static"))
+    fun Application.install() {
+        install(DefaultHeaders)
+        install(CallLogging)
+        install(FreeMarker) {
+            templateLoader = ClassTemplateLoader(MMApp::class.java.classLoader, "templates")
+        }
+
+        install(Locations)
+        install(Routing) {
+            indexRoute()
+            skillsRoute()
+            projectsRoute()
+            learningRoute()
+            route("static") {
+                serveFileSystem(File("src/main/resources/static"))
+            }
         }
     }
-}
 
-
-fun main(args: Array<String>) {
-    val port = Integer.valueOf(System.getenv("PORT") ?: "8080")
-    embeddedServer(Netty, port, reloadPackages = listOf("heroku"), module = Application::install).start()
 }
